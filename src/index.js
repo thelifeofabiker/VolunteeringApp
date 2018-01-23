5f3f4849c51ae8b0d7972b36a3b23311172fcc6e
Sylvia Cipra <sylvia8590@gmail.com>
2018-01-23 18:49:37 -0500

    added render to #app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route } from 'react-router-dom';
-import './css/main.css';
 import VolunteerEvents from './components/VolunteerEvents';
 
 
-render( <VolunteerEvents/>, document.querySelector('#location-form') );
+render( <VolunteerEvents/>, document.querySelector('#app') );
