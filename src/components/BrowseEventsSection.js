1125b014a69562a503206c304803ff7cbe0e2032
Sylvia Cipra <sylvia8590@gmail.com>
2018-01-23 18:51:27 -0500

    added temporary render

--- src/components/BrowseEventsSection.js
+++ src/components/BrowseEventsSection.js
@@ -1,2 +1,10 @@
 //section below the location form where user can view upcoming volunteer events.
 import React from 'react';
+
+class BrowseEventsSection extends React.Component {
+  render() {
+    return false
+  }
+}
+
+export default BrowseEventsSection;
