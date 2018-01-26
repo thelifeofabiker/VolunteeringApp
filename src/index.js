818e4f1fe391f34f2827fe5a837a0fdb8bdae01c
Sylvia Cipra <sylvia8590@gmail.com>
2018-01-25 22:19:37 -0500

    saved changes to index.js

--- src/index.js
+++ src/index.js
@@ -13,13 +13,7 @@ const Root = () => {
   return (
     <BrowserRouter>
       <div>
-<<<<<<< HEAD
-        <NavigationBar/>
-        <Route exact path="/Home" component={ Homepage } />
-=======
-        <NavBar/>
-        <Route exact path="/" component={ Homepage } />
->>>>>>> 0f24a17cbfa691beb3e39745d3d1dce55dcd8f8b
+        // <NavigationBar/>
         <Route path="/Volunteer-Events" component={ VolunteerEvents } />
         <Route path="/For-Organizers" component={ SubmitForm } />
       </div>
